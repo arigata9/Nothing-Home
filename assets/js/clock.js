1b3778887c1551b6d8f5ad24328a7a79ab9a8fef
arigata9 <markus@arigata9.de>
2023-10-03 00:55:59 +0200

    Change timeout  and fixing two digit hours

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -2,13 +2,13 @@ window.onload = displayClock;
 
 function displayClock() {
     var d = new Date();
-    var dd = d.getDate();
     var min = (mins = ('0' + d.getMinutes()).slice(-2));
-    var hh = d.getHours();
+    var hh = (hours = ('0' + d.getHours()).slice(-2));
+    
 
     document.getElementById('hour').innerText = hh;
     document.getElementById('separator').innerHTML = '- -';
     document.getElementById('minutes').innerText = min;
 
-    setTimeout(displayClock, 1000);
+    setTimeout(displayClock, 5000);
 }
