c20b448a4ee68cab56aad6820e45b16ed1732426
arigata9 <markus@arigata9.de>
2023-10-03 20:25:01 +0200

    Example file

---
+++ example-config.js
@@ -0,0 +1,52 @@
+const CONFIG = {
+
+	// General
+	name: 'John',
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good Afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to sleep!',
+
+    // Buttons
+
+	buttonsContainer: [
+		{
+			name: 'Github',
+			icon: 'github',
+            iconstyle: 'brands',
+			link: 'https://github.com/',
+		},
+		{
+			name: 'Mail',
+			icon: 'envelope',
+            iconstyle: 'regular',
+			link: 'https://mail.google.com',
+		},
+        {
+			name: 'Calendar',
+			icon: 'calendar',
+            iconstyle: 'regular',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			name: 'Twitter',
+			icon: 'twitter',
+            iconstyle: 'brands',
+			link: 'https://twitter.com',
+		},
+		{
+			name: 'Reddit',
+			icon: 'reddit-alien',
+            iconstyle: 'brands',
+			link: 'https://reddit.com',
+		},
+		{
+			name: 'Youtube',
+			icon: 'youtube',
+            iconstyle: 'brands',
+			link: 'https://youtube.com/',
+		},
+	],
+}
